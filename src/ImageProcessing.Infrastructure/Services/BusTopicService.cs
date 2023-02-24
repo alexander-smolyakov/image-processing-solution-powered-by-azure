7cd03e4fc321f738fd1bb0d9419cc1eb605ca83b
Alexander Smolyakov <alexander.smolyakov.dev@gmail.com>
2023-02-24 14:27:05 +0400

    Add `BusTopicService` class

---
+++ src/ImageProcessing.Infrastructure/Services/BusTopicService.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using Azure.Messaging.ServiceBus;
+using ImageProcessing.Core.Entities;
+
+namespace ImageProcessing.Infrastructure.Services
+{
+    public class BusTopicService : IBusTopicService
+    {
+        private readonly ServiceBusClient _busClient;
+        private readonly ServiceBusSender _busSender;
+
+        public BusTopicService(string connectionString, string topicName)
+        {
+            _busClient = new ServiceBusClient(connectionString);
+            _busSender = _busClient.CreateSender(topicName);
+        }
+
+        public async Task SendMessagesAsync(ProcessingTask task)
+        {
+            var message = new ServiceBusMessage(task.Id.ToString());
+            await _busSender.SendMessageAsync(message);
+        }
+    }
+}
